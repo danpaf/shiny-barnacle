918c9ec10f7bda40e5a8e2407a0cca54000fe0ba
zxceb <d4npaf@ya,ru>
2022-11-15 00:12:24 +0300

    start

---
+++ ConsoleApp1/commandsSlash.cs
@@ -0,0 +1,11 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+public class commandsSlash : BaseCommandModule
+{
+    [Command("greet")]
+    public async Task GreetCommand(CommandContext ctx)
+    {
+        await ctx.RespondAsync("Greetings! Thank you for executing me!");
+    }
+}
