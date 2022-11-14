bfd3016bc6b4c2356188958434d8599ef5ea5ab8
zxceb <d4npaf@ya,ru>
2022-11-15 00:16:36 +0300

    start

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(token:"MTA0MTgwOTg1NzQzMjUyNjg5OQ.GiPHha.hr06vc9ivmWkATrsmYCsi4uAJJJHLmTRUeUA1Y").GetAwaiter().GetResult();
+        }
+
+        private string prefix;
+        static async Task MainAsync(string token)
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = token,
+                TokenType = TokenType.Bot,
+                
+            });
+            var commands = discord.UseCommandsNext(new CommandsNextConfiguration());
+            commands.RegisterCommands<commandsSlash>();
+            new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] {"/"}
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
